7dd64c987a6b807ce3919b5cfc848de1578868ba
Emile Sabatier <dev@emilesabatier.com>
2024-04-24 22:32:12 +0200

    update script

--- bin/root.security.sh
+++ bin/root.security.sh
@@ -4,24 +4,6 @@ printf "\n"
 printf "\e[1;31m[Security]\e[0m"
 
 
-# Update packages
-apt update
-apt upgrade
-
-# Set up new user
-adduser --force-badname toor
-apt -y install sudo
-usermod -aG root,sudo,adm toor
-cp -fv /home/toor/.sh/etc/ssh/sshd_config /etc/ssh/sshd_config
-systemctl restart sshd
-
-# Install git
-apt -y install git
-
-# Pull repo
-git clone --depth 1 https://github.com/emilesabatier/sh /home/toor/.sh
-
-
 # Set up iptables
 apt -y install iptables
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
--- bin/root.setup.sh
+++ bin/root.setup.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 
 
+# Update packages
+apt update
+apt upgrade
+
+# Set up new user
+adduser --force-badname toor
+apt -y install sudo
+usermod -aG root,sudo,adm toor
+cp -fv /home/toor/.sh/etc/ssh/sshd_config /etc/ssh/sshd_config
+systemctl restart sshd
+
+# Install git
+apt -y install git
+
+# Pull repo
+git clone --depth 1 https://github.com/emilesabatier/sh /home/toor/.sh
+
 # Start scripts
 sh /home/toor/.sh/bin/root.security.sh
 sh /home/toor/.sh/bin/root.docker.sh
