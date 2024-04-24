25e0f09ce6ee6afc07f0460264ec85104a3174ff
Emile Sabatier <dev@emilesabatier.com>
2024-04-24 22:16:32 +0200

    update script

--- bin/toor.traefik.sh
+++ bin/toor.traefik.sh
@@ -5,6 +5,6 @@ echo "\e[1;31m[Traefik]\e[0m"
 
 
 # Add traefik compose file
-mkdir ~/traefik
-sudo cp /root/.sh/home/traefik/docker-compose.yml ~/traefik/docker-compose.yml
+mkdir traefik
+sudo cp /root/.sh/home/traefik/docker-compose.yml traefik/docker-compose.yml
 docker compose -f ~/traefik/docker-compose.yml up -d
