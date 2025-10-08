f939c115bcec7516203da9fe7d5b6f7963a0aa5c
&mile <dev@emilesabatier.com>
2025-10-09 01:38:51 +0200

    Update toor.traefik.sh

--- bin/toor.traefik.sh
+++ bin/toor.traefik.sh
@@ -7,4 +7,5 @@ echo "\e[1;31m[Traefik]\e[0m"
 # Add traefik compose file
 mkdir /home/toor/traefik
 cp /home/toor/.sh/home/traefik/compose.yaml /home/toor/traefik/compose.yaml
+cp /home/toor/.sh/home/traefik/config.yaml /home/toor/traefik/config.yaml
 docker compose -f /home/toor/traefik/compose.yaml up -d
