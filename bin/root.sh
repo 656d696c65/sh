acc2414b651673e53b280089dbe567d6f2abd97f
&mile <dev@emilesabatier.com>
2025-10-09 01:34:38 +0200

    Update root.sh

--- bin/root.sh
+++ bin/root.sh
@@ -37,20 +37,21 @@ echo "\n\e[1;31m[Docker]\e[0m"
 
 
 # Set up Docker's apt repository
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
 sudo apt update
-sudo apt install -y ca-certificates curl
+sudo apt install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-sudo echo \
+echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 
-# Install the Docker packages
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Add Docker rights
 sudo groupadd docker
